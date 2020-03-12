2457ffd7c3dde465cd50d7001bcd84f50f55ffc1
Keenan <keenanhallas@gmail.com>
2020-03-11 20:16:25 -0400

    four problems done

--- index.js
+++ index.js
@@ -96,8 +96,13 @@ function temperatureInF(temp, unit) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  const person = {
+    id: id,
+    name: name,
+    email: email
+  };
+  return person;
 }
 
 /**
@@ -113,7 +118,7 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
+function getName(personObject) {
   /* code here */
 }
 
